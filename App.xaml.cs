307c4510824dbbf1c5605c67bae95d9f7f25364a
xiaoming <47764491+xiaoming709394@users.noreply.github.com>
2024-01-04 17:25:41 +0800

    Evan
    初始化

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MultiPlatform
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MultiPlatform
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
