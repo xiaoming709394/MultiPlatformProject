c1cbec61edd82d239cc4afe4e5ac25a4b5aefb70
xiaoming <47764491+xiaoming709394@users.noreply.github.com>
2024-01-05 17:38:18 +0800

    Evan
    添加登录，扫码

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,10 +1,15 @@
-﻿namespace MultiPlatform
+﻿using BlazorHybrid.Maui.Shared;
+
+namespace MultiPlatform
 {
     public partial class MainPage : ContentPage
     {
         public MainPage()
         {
             InitializeComponent();
+
+            //添加初始化BlazorWebView组件 InitBlazorWebView 使用js权限.
+            new InitBlazorWebView(blazorWebView);
         }
     }
 }
